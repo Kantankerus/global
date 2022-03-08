a447f02ac066e989cd3b721e60f56d831ffbee16
Kantankerus <lord.savyj@gmail.com>
2022-03-07 17:02:33 -0700

    Neat.

--- window.js
+++ window.js
@@ -4,8 +4,8 @@ if (typeof window !== "undefined") {
     win = window;
 //} else if (typeof global !== "undefined") {
 //    win = global;
-} else if (typeof self !== "undefined"){
-    win = self;
+//} else if (typeof self !== "undefined"){
+//    win = self;
 } else {
     win = {
         location: {
