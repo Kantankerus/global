52c8122885dea46392120a51b7a9fe55124163de
Kantankerus <lord.savyj@gmail.com>
2022-03-07 17:01:11 -0700

    Comment out global.

--- window.js
+++ window.js
@@ -2,8 +2,8 @@ var win;
 
 if (typeof window !== "undefined") {
     win = window;
-} else if (typeof global !== "undefined") {
-    win = global;
+//} else if (typeof global !== "undefined") {
+//    win = global;
 } else if (typeof self !== "undefined"){
     win = self;
 } else {
